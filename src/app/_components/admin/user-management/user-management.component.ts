315243460aa04c5ac25c2e7cc8b99c7b9d259a68
Colin Chumak <chumakc@pm.me>
2019-09-04 16:31:40 -0400

    Finished work on admin modal

--- src/app/_components/admin/user-management/user-management.component.ts
+++ src/app/_components/admin/user-management/user-management.component.ts
@@ -25,7 +25,7 @@ export class UserManagementComponent implements OnInit {
     this.adminService.getUsersWithRoles().subscribe((users: User[]) => {
       this.users = users;
     }, error => {
-      console.log(error);
+    console.log(error);
     });
   }
 
@@ -35,7 +35,19 @@ export class UserManagementComponent implements OnInit {
       roles: this.getRolesArray(user)
     };
     this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
-    this.bsModalRef.content.closeBtnName = 'Close';
+    this.bsModalRef.content.updateSelectedRoles.subscribe((values) => {
+      const rolesToUpdate = {
+        roleNames: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(() => {
+          user.roles = [...rolesToUpdate.roleNames];
+        }, error => {
+          console.log(error);
+        });
+      }
+    });
   }
 
   private getRolesArray(user) {
